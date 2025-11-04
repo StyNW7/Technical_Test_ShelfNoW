66051aa78eec782d640ba633b742c5eb2d7785db
Stanley Nathanael Wijaya <stanley.n.wijaya7@gmail.com>
2025-11-04 10:39:37 +0700

    cart and hot reload

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { AuthProvider } from './contexts/AuthContext.tsx'
+import { CartProvider } from './lib/cart-context.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider>
-      <App />
+      <CartProvider>
+        <App />
+      </CartProvider>
     </AuthProvider>
   </StrictMode>,
 )
